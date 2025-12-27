1b25fbd9f7998780e9f885470c2e854e182359c5
jonihalimsentrawebmedia <joni.halim@sentrawebmedia.com>
2025-12-27 15:40:13 +0700

    fix declare mettadata

---
+++ src/components/Meta/index.tsx
@@ -0,0 +1,32 @@
+import { Helmet } from 'react-helmet-async'
+
+interface MetaProps {
+  title: string
+  description?: string
+  image?: string
+  url?: string
+}
+
+export const Meta = ({ title, description = 'Website resmi', image, url }: MetaProps) => {
+  return (
+    <Helmet>
+      {/* Basic */}
+      <title>{title}</title>
+      <meta name="description" content={description} />
+
+      {/* Open Graph */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
+
+      {url && <meta property="og:url" content={url} />}
+      {image && <meta property="og:image" content={image} />}
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {image && <meta name="twitter:image" content={image} />}
+    </Helmet>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Providers from '@/provider'
+import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Providers>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </Providers>
   </StrictMode>
 )
