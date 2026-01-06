f0746ed184405b6dc234a1b7a6152fc574380220
jonihalimsentrawebmedia <joni.halim@sentrawebmedia.com>
2026-01-06 16:29:43 +0700

    fix done

--- src/pages/modules/website-utama/component/layout/index.tsx
+++ src/pages/modules/website-utama/component/layout/index.tsx
@@ -7,20 +7,22 @@ export default function MainLayout() {
   const [collapsed, setCollapsed] = useState(false)
 
   return (
-    <div className="h-screen flex flex-col bg-[#F8FFF9]">
-      <Header collapsed={collapsed} setCollapsed={setCollapsed} />
+    <>
+      <div className="h-screen flex flex-col bg-[#F8FFF9]">
+        <Header collapsed={collapsed} setCollapsed={setCollapsed} />
 
-      <div className="flex flex-1 overflow-hidden">
-        <Sidebar collapsed={collapsed} />
+        <div className="flex flex-1 overflow-hidden">
+          <Sidebar collapsed={collapsed} />
 
-        <main className="flex-1 overflow-auto p-4">
-          <Outlet />
-        </main>
+          <main className="flex-1 overflow-auto p-4">
+            <Outlet />
+          </main>
+        </div>
+
+        <footer className="text-center py-2 text-xs text-gray-400 border-t">
+          Admin Website © 2025
+        </footer>
       </div>
-
-      <footer className="text-center py-2 text-xs text-gray-400 border-t">
-        Admin Website © 2025
-      </footer>
-    </div>
+    </>
   )
 }
