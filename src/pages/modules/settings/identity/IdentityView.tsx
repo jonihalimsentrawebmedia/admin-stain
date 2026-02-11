c0decb6462ce99f867f14d2fdf5ab87b40ffba6d
joni halim <joni.halim@sentrawebmedia.com>
2026-02-11 19:47:38 +0700

    fix done

--- src/pages/modules/settings/identity/IdentityView.tsx
+++ src/pages/modules/settings/identity/IdentityView.tsx
@@ -1,17 +1,17 @@
-import { Form } from '@/components/ui/form'
+import {Form} from '@/components/ui/form'
 import IdentityViewModel from './IdentityViewModel'
 import ButtonTitleGroup from '@/components/common/button/ButtonTitleGroup'
 import DetailField from '@/components/common/field/DetailField'
 import TextInput from '@/components/common/form/TextInput'
 import InputImage from '@/components/common/form/InputImage'
 import ButtonForm from '@/components/common/button/ButtonForm'
-import { Skeleton } from '@/components/ui/skeleton'
+import {Skeleton} from '@/components/ui/skeleton'
 
 const IdentityView = () => {
-  const { field, form, formDetail, handleSave, isEdit, loading, loadingDetail, setIsEdit } =
+  const {field, form, formDetail, handleSave, isEdit, loading, loadingDetail, setIsEdit} =
     IdentityViewModel()
   if (loadingDetail) {
-    return <Skeleton className="w-full h-[100px]" />
+    return <Skeleton className="w-full h-[100px]"/>
   }
   return (
     <div className="flex flex-col gap-4">
@@ -22,40 +22,41 @@ const IdentityView = () => {
             buttonGroup={
               isEdit
                 ? [
-                    {
-                      label: 'cancel',
-                      onClick: () => {
-                        setIsEdit(false)
-                      },
-                      type: 'cancel',
+                  {
+                    label: 'cancel',
+                    onClick: () => {
+                      setIsEdit(false)
                     },
-                    {
-                      label: 'Simpan',
-                      onClick: () => {},
-                      type: 'save',
-                      isDisabled: loading,
+                    type: 'cancel',
+                  },
+                  {
+                    label: 'Simpan',
+                    onClick: () => {
                     },
-                  ]
+                    type: 'save',
+                    isDisabled: loading,
+                  },
+                ]
                 : [
-                    {
-                      label: 'Edit Data',
-                      onClick: (e) => {
-                        e.preventDefault()
-                        setIsEdit(true)
-                      },
-                      type: 'edit',
+                  {
+                    label: 'Edit Data',
+                    onClick: (e) => {
+                      e.preventDefault()
+                      setIsEdit(true)
                     },
-                  ]
+                    type: 'edit',
+                  },
+                ]
             }
           />
           {isEdit ? (
             <div className="flex flex-col gap-4">
-              <TextInput form={form} name="nama" label="Nama" />
-              <InputImage form={form} name="logo" label="Logo" />
-              <InputImage form={form} name="background" label="Background" />
+              <TextInput form={form} name="nama" className={'text-black'} label="Nama"/>
+              <InputImage form={form} name="logo" label="Logo"/>
+              <InputImage form={form} name="background" label="Background"/>
             </div>
           ) : (
-            <DetailField isRow={false} data={field} form={formDetail} />
+            <DetailField isRow={false} data={field} form={formDetail}/>
           )}
           {isEdit && (
             <ButtonForm
