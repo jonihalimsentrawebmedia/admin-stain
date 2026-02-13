33acd450602ce39ca840340d87dd8f289cfd1de8
jonihalimsentrawebmedia <joni.halim@sentrawebmedia.com>
2026-02-13 08:17:46 +0700

    fix done

--- src/pages/modules/website-lembaga/layanan/aim/template/model/index.tsx
+++ src/pages/modules/website-lembaga/layanan/aim/template/model/index.tsx
@@ -1,31 +1,32 @@
 export interface DocumentSupportList {
-  id_lembaga_template_aim: string;
-  id_satuan_organisasi: string;
-  judul: string;
-  slug: string;
-  urutan: number;
-  created_at: string;
-  created_user: string;
-  updated_at: string;
-  updated_user: string;
-  nama_user_created: string;
-  nama_user_updated: string;
-  jumlah_aim: number;
+  id_lembaga_daftar_dokumen: string
+  id_lembaga_template_aim: string
+  id_satuan_organisasi: string
+  judul: string
+  slug: string
+  urutan: number
+  created_at: string
+  created_user: string
+  updated_at: string
+  updated_user: string
+  nama_user_created: string
+  nama_user_updated: string
+  jumlah_aim: number
 }
 
 export interface DokumenTemplateAim {
-  id_lembaga_dokumen_template_aim: string;
-  id_satuan_organisasi: string;
-  id_lembaga_template_aim: string;
-  nama_dokumen: string;
-  slug: string;
-  url: string;
-  urutan: number;
-  created_at: string;
-  created_user: string;
-  updated_at: string;
-  updated_user: string;
-  nama_user_created: string;
-  nama_user_updated: string;
-  public:boolean
+  id_lembaga_dokumen_template_aim: string
+  id_satuan_organisasi: string
+  id_lembaga_template_aim: string
+  nama_dokumen: string
+  slug: string
+  url: string
+  urutan: number
+  created_at: string
+  created_user: string
+  updated_at: string
+  updated_user: string
+  nama_user_created: string
+  nama_user_updated: string
+  public: boolean
 }
