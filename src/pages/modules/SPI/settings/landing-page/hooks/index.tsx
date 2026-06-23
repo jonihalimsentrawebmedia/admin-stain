76c88fff02f5d6cef9bb5446e44329fcfae66c2f
joni Halim <joni.halim@sentrawebmedia.com>
2026-06-23 15:06:50 +0700

    fix PPT bug crash app

--- src/pages/modules/SPI/settings/landing-page/hooks/index.tsx
+++ src/pages/modules/SPI/settings/landing-page/hooks/index.tsx
@@ -1,15 +1,9 @@
-import { useEffect, useState } from 'react'
-import type { Meta } from '@/components/common/table/TablePagination.tsx'
 import { useQuery } from '@tanstack/react-query'
 import AxiosClient from '@/provider/axios.tsx'
-import type { IUnitLandingPage } from '../data/types'
 import type { BasicProps } from '@/utils/globalType.ts'
 
 export const UseGetUnitLandingPage = (props?: BasicProps) => {
   const { page, limit, search } = props ?? {}
-
-  const [unitLanding, setUnitLanding] = useState<IUnitLandingPage[]>([])
-  const [meta, setMeta] = useState<Meta>()
 
   const ParamsSearch = new URLSearchParams()
   if (page) ParamsSearch.append('page', page ?? '0')
@@ -24,12 +18,5 @@ export const UseGetUnitLandingPage = (props?: BasicProps) => {
 
   const loading = isLoading || isFetching
 
-  useEffect(() => {
-    if (data) {
-      setUnitLanding(data?.data)
-      setMeta(data?.meta)
-    }
-  }, [data])
-
-  return { unitLanding, loading, meta }
+  return { unitLanding: data?.data ?? [], loading, meta: data?.meta }
 }
